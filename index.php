40af19320e51b0d2bfc7081ca16406b660d5d73d
raulxlolx <91206628+raulxlolx@users.noreply.github.com>
2023-11-09 10:42:32 +0100

    cxzc

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
-<?php 
-
-
+<?php
 include("users.php");
-$sql="SELECT * FROM empleados";
-$result=mysqli_query($conn,$sql) or die (mysqli_error($conn));
-
+$sql = "SELECT * FROM empleados";
+$result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
 ?>
 
 <!DOCTYPE html>
@@ -15,25 +12,25 @@ $result=mysqli_query($conn,$sql) or die (mysqli_error($conn));
     <title>Document</title>
 </head>
 <body>
+    <h2>Usuarios</h2>
     <table>
-        <th colspan="3"><h2>Usuarios</h2></th>
         <tr>
-        <th>Nombre</th>
-        <th>Apellido</th>
-        <th>Edad</th>
-        <th>categoria</th>
-        <th>salario</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Edad</th>
+            <th>Categoría</th>
+            <th>Salario</th>
         </tr>
 
-        <?php while($row=mysqli_fetch_array($result)){?>
-        <tr>
-        <td><?php echo $row['nombre'];?></td>
-        <td><?php echo $row['apellido'];?></td>
-        <td><?php echo $row['edad'];?></td>
-        <td><?php echo $row['categoria'];?></td>
-        <td><?php echo $row['salario'];?></td>
-        </tr>
-        <?php }?>
+        <?php while ($row = mysqli_fetch_array($result)) { ?>
+            <tr>
+                <td><?php echo $row['nombre']; ?></td>
+                <td><?php echo $row['apellido']; ?></td>
+                <td><?php echo $row['edad']; ?></td>
+                <td><?php echo $row['categoria']; ?></td>
+                <td><?php echo $row['salario']; ?></td>
+            </tr>
+        <?php } ?>
     </table>
 </body>
 </html>
--- users.php
+++ users.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
+$conn = new mysqli("192.168.1.248", "root", "3216", "empresa");
 
-$conn=new msqli("192.168.1.248","root","3216","empresa");
-$conn->query("SET NAMES 'UTF8' ");
-if(!$conn){
-
-die("connection failed : " .$conn->connect_error);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+$conn->set_charset('utf8');
 ?>
