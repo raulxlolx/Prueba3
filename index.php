0775e8a7da958af9f9033cdb4b944f23a5559c9e
raulxlolx <91206628+raulxlolx@users.noreply.github.com>
2023-11-23 11:13:17 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,164 +1,160 @@
 <?php
-$server = "127.0.0.1";
-$user = "base";
-$pass = "3216";
-$database = "empresa";
-
-$conn = new mysqli($server, $user, $pass, $database);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$conn->set_charset('utf8');
-
-$sql = "SELECT * FROM empleados";
-$result = $conn->query($sql);
-
-if (!$result) {
-    die("Error en la consulta: " . $conn->error);
-}
-?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Usuarios</title>
+    <title>Tabla de usuarios</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+        h2 {
+            margin-top: 20px;
+        }
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+        form {
+            margin-top: 20px;
+        }
+        label {
+            display: block;
+            margin-bottom: 5px;
+        }
+        input[type="text"] {
+            width: 100%;
+            padding: 5px;
+            margin-bottom: 10px;
+        }
+        input[type="submit"] {
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            cursor: pointer;
+        }
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+    </style>
 </head>
 <body>
-
-<h2>Usuarios</h2>
-
-<table border="1">
-    <tr>
-        <th>ID</th>
-        <th>Nombre</th>
-        <th>Apellido</th>
-        <th>Edad</th>
-        <th>Categoría</th>
-        <th>Salario</th>
-    </tr>
-
-    <?php while ($row = $result->fetch_assoc()) { ?>
-        <tr>
-            <td><?php echo $row['id']; ?></td>
-            <td><?php echo $row['nombre']; ?></td>
-            <td><?php echo $row['apellido']; ?></td>
-            <td><?php echo $row['edad']; ?></td>
-            <td><?php echo $row['categoria']; ?></td>
-            <td><?php echo $row['salario']; ?></td>
-            <td><a href="?eliminar=<?php echo $row['id'] ?>">Eliminar</a></td>
-       
-            
-        </tr>
-    <?php } ?>
-
-</table>
-
-
-<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-
-<h2>Agregar un nuevo usuario</h2>
-
-<label for="nombre">Ingrese La id</label>
-<input name="id" type="number" >
-<br>
-<label for="nombre">Ingrese Su nombre</label>
-<input name="nombre" type="text" >
-<br>
-<label for="apellido">Ingresa su apellido</label>
-<input type="text" name="apellido">
-<br>
-<label for="edad">Ingrese su edad</label>
-<input name="edad" type="number">
-<br>
-<label for="categoria">Ingrese su categoria</label>
-<input name="categoria" type="text">
-<br>
-<label for="salario">Ingrese su salario</label>
-<input name="salario" type="number"><br>
-<input type="submit" value="Enviar">
-
-</form>
-
-
-<h2>Editar empleado</h2>
-
-<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    <input type="" name="id" value="<?php echo $empleado['id']; ?>">
-    <label for="nombre">Nombre:</label>
-    <input name="nombre" type="text" value="<?php echo $empleado['nombre']; ?>">
-    <br>
-    <label for="apellido">Apellido:</label>
-    <input type="text" name="apellido" value="<?php echo $empleado['apellido']; ?>">
-    <br>
-    <label for="edad">Edad:</label>
-    <input name="edad" type="number" value="<?php echo $empleado['edad']; ?>">
-    <br>
-    <label for="categoria">Categoría:</label>
-    <input name="categoria" type="text" value="<?php echo $empleado['categoria']; ?>">
-    <br>
-    <label for="salario">Salario:</label>
-    <input name="salario" type="number" value="<?php echo $empleado['salario']; ?>"><br>
-    <input type="submit" name="actualizar" value="Actualizar">
-</form>
-
-<?php 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $id= $_POST['id'];
-    $nombre = $_POST['nombre'];
-    $apellido = $_POST['apellido'];
-    $edad = $_POST['edad'];
-    $categoria = $_POST['categoria'];
-    $salario = $_POST['salario'];
-
-    $sql = "INSERT INTO empleados (id, nombre, apellido, edad, categoria, salario) VALUES ('$id', '$nombre', '$apellido', '$edad', '$categoria', '$salario')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Nuevo usuario agregado";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+    <?php
+    // Datos de conexión a la base de datos
+    $host = "localhost"; // Cambia a la dirección de tu servidor MySQL
+    $usuario = "base";
+    $contrasena = "3216";
+    $base_de_datos = "usuarios";
+    // Conexión a la base de datos
+    $conn = new mysqli($host, $usuario, $contrasena, $base_de_datos);
+    // Verificar la conexión
+    if (!$conn) {
+        die("Error en la conexión: " . mysqli_connect_error());
     }
-}
-
-if (isset($_GET['actualizar'])) {
-    $idEmpleadoEditar = $_GET['id'];
-
-    // Obtener los datos del empleado a partir de su ID
-    $sql = "SELECT * FROM empleados WHERE id = '$idEmpleadoEditar'";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        $empleado = $result->fetch_assoc();
+    // Consulta SQL para obtener datos de la tabla
+    $sql = "SELECT * FROM usuarios"; // Cambia 'usuarios' al nombre de tu tabla
+    $result = mysqli_query($conn, $sql);
+    // Procesar y mostrar los datos en una tabla
+    if (mysqli_num_rows($result) > 0) {
+        echo "<table>";
+        echo "<tr><th>Nombre</th><th>Correo</th><th>Acciones</th></tr>";
+        while ($fila = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            echo "<td>" . $fila["nombre"] . "</td>";
+            echo "<td>" . $fila["email"] . "</td>";
+            echo "<td><a href='?id=" . $fila["id"] . "'>Eliminar</a> | <a href='editar.php?id=" . $fila["id"] . "'>Editar</a></td>";
+            echo "</tr>";
+        }
+        echo "</table>";
     } else {
-        echo "No se encontró el empleado con el ID: $idEmpleadoEditar";
+        echo "No se encontraron resultados en la tabla.";
     }
-}
-
-// Actualizar los datos del empleado
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['actualizar'])) {
-    $id = $_POST['id'];
-    $nombre = $_POST['nombre'];
-    $apellido = $_POST['apellido'];
-    $edad = $_POST['edad'];
-    $categoria = $_POST['categoria'];
-    $salario = $_POST['salario'];
-
-    $sql = "UPDATE empleados SET nombre = '$nombre', apellido = '$apellido', edad = '$edad', categoria = '$categoria', salario = '$salario' WHERE id = '$id'";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Empleado actualizado correctamente";
-    } else {
-        echo "Error al actualizar el empleado: " . $conn->error;
+    if(isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $sql = "DELETE FROM usuarios WHERE id = $id";
+        if (mysqli_query($conn, $sql)) {
+            echo "Registro eliminado correctamente.";
+            // Redireccionar a la página actual para evitar la creación de un nuevo usuario al refrescar la página
+            header("Location: " . $_SERVER['PHP_SELF']);
+            exit();
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
     }
-}
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $nombre = $_POST['nombre'];
+        $correo = $_POST['email'];
+        $sql = "INSERT INTO usuarios (nombre, email) VALUES ('$nombre', '$correo')";
+        if (mysqli_query($conn, $sql)) {
+            echo "Nuevo registro creado correctamente.";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+    }
 
-?>
+    // Cerrar la conexión
+    mysqli_close($conn);
+    ?>   
+    <form method="post" action="<?php echo $_SERVER ['PHP_SELF']; ?>" aling="center">
+     <h2>Agregar nuevo usuario</h2>   
+        <label for ="nombre">Nombre:</label><br>
+        <input type="text" name="nombre" id="nombre"><br>
+        <br>
+        <label for ="correo">Correo:</label><br>
+        <input type="text" name="email" id="email"><br>
+        <br>
+        <input type="submit" value="Agregar">
+    </form>
 
+    <?php
+    if(isset($_GET['edit'])) {
+        $id = $_GET['edit'];
+        $sql = "SELECT * FROM usuarios WHERE id = $id";
+        $result = mysqli_query($conn, $sql);
+        if(mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            $nombre = $row['nombre'];
+            $correo = $row['email'];
+            echo "<h2>Editar usuario</h2>";
+            echo "<form method='post' action='editar.php'>";
+            echo "<input type='hidden' name='id' value='$id'>";
+            echo "<label for='nombre'>Nombre:</label><br>";
+            echo "<input type='text' name='nombre' id='nombre' value='$nombre'><br>";
+            echo "<br>";
+            echo "<label for='correo'>Correo:</label><br>";
+            echo "<input type='text' name='correo' id='email' value='$correo'><br>";
+            echo "<br>";
+            echo "<input type='submit' value='Guardar'>";
+            echo "</form>";
+        } else {
+            echo "No se encontró el usuario.";
+        }
+    }
 
-
-
-
-
+    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['id'])) {
+        $id = $_POST['id'];
+        $nombre = $_POST['nombre'];
+        $correo = $_POST['email'];
+        $sql = "UPDATE usuarios SET nombre='$nombre', correo='$correo' WHERE id=$id";
+        if(mysqli_query($conn, $sql)) {
+            echo "Usuario actualizado correctamente.";
+        } else {
+            echo "Error al actualizar el usuario: " . mysqli_error($conn);
+        }
+    }
+    ?>
 </body>
 </html>
