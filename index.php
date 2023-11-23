d577505b5811c46b8929ca73e7b4c40f7fe929da
raulxlolx <91206628+raulxlolx@users.noreply.github.com>
2023-11-23 09:55:38 +0100

    asdad

--- index.php
+++ index.php
@@ -1,86 +1,48 @@
 
-
+<?php
 $server = "127.0.0.1";
-
 $user = "base";
-
 $pass = "3216";
-
 $database = "empresa";
-
-
 
 $conn = new mysqli($server, $user, $pass, $database);
 
-
-
 if ($conn->connect_error) {
-
     die("Connection failed: " . $conn->connect_error);
-
 }
-
-
 
 $conn->set_charset('utf8');
 
-
-
 $sql = "SELECT * FROM empleados";
+$result = $conn->query($sql);
 
-$result = mysqli_query($conn, $sql) or die("Error en la consulta: " . mysqli_error($conn));
-
+if (!$result) {
+    die("Error en la consulta: " . $conn->error);
+}
 ?>
-
-
 
 <h2>Usuarios</h2>
 
 <table border="1">
-
     <tr>
-
         <th>Nombre</th>
-
         <th>Apellido</th>
-
         <th>Edad</th>
-
         <th>Categoría</th>
-
         <th>Salario</th>
-
     </tr>
 
-
-
-    <?php while ($row = mysqli_fetch_array($result)) { ?>
-
+    <?php while ($row = $result->fetch_assoc()) { ?>
         <tr>
-
             <td><?php echo $row['nombre']; ?></td>
-
             <td><?php echo $row['apellido']; ?></td>
-
             <td><?php echo $row['edad']; ?></td>
-
             <td><?php echo $row['categoria']; ?></td>
-
             <td><?php echo $row['salario']; ?></td>
-
         </tr>
-
-
-
     <?php } ?>
-
-
 
 </table>
 
-
-
 </body>
-
 </html>
-
